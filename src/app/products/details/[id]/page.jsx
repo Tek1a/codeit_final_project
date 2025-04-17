6a163fa776cfdf77681032aac780085f90e631f8
Tek1a <teklabluashvili2008@gmail.com>
2025-04-17 22:01:05 +0400

    adding products details page

---
+++ src/app/products/details/[id]/page.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const Page = async ({ params }) => {
+    const id = params.id;
+    console.log(params);
+    let product;
+    try {
+      const data = await fetch(`https://fakestoreapi.com/products/${id}`);
+      product = await data.json();
+    } catch (error) {
+      throw Error(error);
+    }
+    console.log("prefetched");
+
+    return (
+      <div className={styles.modal}>
+        <div className={styles.modalContent}>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={200}
+            height={200}
+            className={styles.productImage}
+          />
+          <div className={styles.textContent}>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p><strong>Price:</strong> ${product.price}</p>
+            <p><strong>Category:</strong> {product.category}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+export default Page;
